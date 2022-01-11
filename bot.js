a30702aba7e80491d840d26087748459b5db82d8
martian17 <yakumodanshi@gmail.com>
2022-01-11 16:37:42 +0900

    setup the bot

---
+++ bot.js
@@ -0,0 +1,111 @@
+/*
+let isntEmpty = function(obj){
+    for(let key in obj){
+        return true;
+    }
+    return false;
+};*/
+
+
+let min = function(a,b){
+    return a < b ? a : b;
+};
+
+
+
+
+let Cmds = function(str){
+    this.name = str;
+    let maxsublen = 0;
+    this.sub = function(name){
+        //console.log(name,name.length);
+        if(maxsublen < name.length){
+            maxsublen = name.length;
+        }
+        if(!(name in this.subcmds)){
+            this.subcmds[name] = new Cmds(name);
+        }
+        return this.subcmds[name];
+    };
+    this.subcmds = {};
+    this.always = [];//executes no matter what
+    this.tops = [];//top function only responds to tail call
+    this.addFunc = function(cb,always){
+        if(always){
+            this.always.push(cb);
+        }else{
+            this.tops.push(cb);
+        }
+    };
+    
+    this.call = function(msg,substr){
+        //console.log(substr);
+        this.always.map(cb=>cb(msg,substr));
+        //parsing the substr
+        let b = (substr.match(/^\s+/)||"").length;
+        let token = "";
+        let ii = 0;
+        //console.log(maxsublen,b,min(substr.length,b+maxsublen));
+        for(let i = b; i < min(substr.length,b+maxsublen+1); i++){
+            if(substr[i].match(/\s/)){//found the limit
+                ii = i;
+                token = substr.slice(b,ii);
+                //console.log("matched token",token);
+                break;
+            }else if(i+1 === substr.length){
+                //end of the string, token found
+                ii = i+1;
+                token = substr.slice(b,ii);
+                //console.log("matched end",token);
+                break;
+            }
+        }
+        //console.log(token,ii);
+        if(token && token in this.subcmds){//sub command found
+            let substr1 = substr.slice(ii);
+            //console.log(token,substr1);
+            this.subcmds[token].call(msg,substr1);
+        }else{
+            this.tops.map(cb=>cb(msg,substr));
+        }
+    };
+};
+
+
+
+//re-factored the bot
+//bot is now a client wrapper
+let Bot = function(client,prefix){
+    this.client = client;
+    let that = this;
+    this.prefix = prefix||"/";
+    
+    let cmds = new Cmds();
+    this.sub = cmds.sub.bind(cmds);
+    
+    //readyness schenanigen
+    this.ready = false;
+    let readyFuncs = [];
+    this.onReady = function(cb){
+        if(this.ready){
+            cb();
+        }else{
+            readyFuncs.push(cb);
+        }
+    };
+    client.on("ready", () => {
+        that.isReady = true;
+        readyFuncs.map(cb=>cb());
+    });
+    
+    
+    client.on("message", async msg => {
+        let str = msg.content.trim();
+        if(str.slice(0,that.prefix.length) === that.prefix){//if equal to prefix
+            str = str.slice(that.prefix.length).trim();
+            cmds.call(msg,str);
+        }
+    });
+};
+
+module.exports = Bot;
---
+++ main.js
@@ -0,0 +1,82 @@
+const Discord = require("discord.js");
+
+let getClient = function(Discord){
+    let flags = Discord.Intents.FLAGS;
+    const client = new Discord.Client({
+        intents: [
+            flags.GUILDS, flags.GUILD_MESSAGES
+        ] /*["GUILDS", "GUILD_MESSAGES"]*/
+    });
+    
+    /*
+    let wrapper = Object.create(client);
+    
+    let evts = {};
+    let targets = "ready,message".split(",");
+    targets.map(t=>{
+        evts[t] = [];
+        client.on(t,function(a,b,c,d,e,f){
+            evts[t].map(cb=>{
+                console.log("cb called");
+                cb(a,b,c,d,e,f)
+            });
+        });
+    });
+    client.on = function(evt,cb){
+        evts[evt].push(cb)
+    };*/
+    return client;
+};
+
+
+const client = getClient(Discord);
+require('dotenv').config();
+console.log(process.env.TOKEN);
+client.login(process.env.TOKEN);
+
+
+let Bot = require("./bot.js");
+
+let main = async function(){
+    let bot = (new Bot(client,"."));
+    
+    
+    let initmsgs = [];
+    bot.onReady(()=>{
+        console.log(`Logged in as ${client.user.tag}!`);
+        const guilds = bot.client.guilds.cache;
+        initmsgs.push("guilds: "+JSON.stringify(guilds.map(g=>g.name)));
+        //sending it to every channels
+        guilds.map(guild=>{
+            //GUILD_CATEGORY
+            //GUILD_CATEGORY
+            //GUILD_TEXT
+            //GUILD_VOICE
+            guild.channels.cache.filter(channel=>{
+                channel.type === "GUILD_TEXT";
+            }).map(channel=>{
+                console.log(channel.type);
+                channel.send(initmsgs.join("\n"));
+            });
+        });
+    });
+    
+    bot.sub("run").addFunc((msg,substr)=>{
+        let code = substr.trim();
+        let lang = "js"
+        if(code.slice(0,3) === "```" && code.slice(-3) === "```"){
+            code = code.slize(3,-3);
+            let lines = code.split("\n");
+            if(lines.length > 1){
+                if(lines[0].length !== 0)lang = lines[0].trim();
+                lines = lines.slice(1);
+            }
+            code = lines.join("\n");
+        }else if(code.slice(0,1) === "`" && code.slice(-1) === "`"){
+            code = code.slize(3,-3);
+        }
+        msg.reply("running the code\n"+`\`\`\`${lang}\n${code}\`\`\``);
+    });
+};
+
+main();
